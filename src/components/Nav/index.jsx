cf069ba02aa6f2dff430951f26c5bed5f5026140
Lukman <Lukmansanni60@gmail.com>
2022-08-11 18:46:59 +0100

    add responsiveness `Nav` component

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,13 +1,16 @@
 import { ReactComponent as HeroIcon } from 'assets/icons/hero-icon.svg';
+import { ReactComponent as MenuIcon } from 'assets/icons/menu.svg';
 import { Button } from 'components';
+import { useState } from 'react';
 
 export const Nav = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
   return (
-    <nav className="bg-white rounded-2xl mx-16 flex items-center py-4 px-12 justify-between">
+    <nav className="bg-white fixed xs:relative left-0 transition duration-300 w-[100vw] xs:w-[unset] xs:rounded-2xl xs:mx-4 900:mx-7 1100:mx-16 flex items-center py-4 px-8 800:px-6 1100:px-16 justify-between">
       <a href="#">
-        <HeroIcon className="cursor-pointer" />
+        <HeroIcon className="cursor-pointer w-20 xs:w-[unset]" />
       </a>
-      <ul className="flex gap-x-8 text-sm">
+      <ul className="gap-x-8 text-sm hidden 800:flex">
         <li className="hover:text-orange transition duration-300">
           <a href="#">About</a>
         </li>
@@ -24,12 +27,13 @@ export const Nav = () => {
           <a href="#">Speak to us</a>
         </li>
       </ul>
-      <div className="flex items-center">
+      <div className="flex items-center hidden 800:block">
         <a href="#" className="mr-4">
           Sign in
         </a>
         <Button>Sign up</Button>
       </div>
+      <MenuIcon className="800:hidden h-8" onClick={() => setDrawerOpen(true)} />
     </nav>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,8 +3,8 @@ import { Nav } from 'components';
 export const Home = () => {
   return (
     <>
-      <header className="bg-hero-bg bg-no-repeat bg-center bg-cover h-[100vh] w-[100%] pt-3 px-5">
-        <div className="bg-orange h-[100%] w-[100%] rounded-[10px] bg-opacity-[0.04] pt-2">
+      <header className="bg-hero-bg bg-no-repeat bg-center bg-cover h-[100vh] w-[100%] xs:pt-3 px-3 xs:px-5">
+        <div className="bg-orange h-[100%] w-[100%] rounded-[10px] bg-opacity-[0.04] xs:pt-2">
           <Nav />
         </div>
       </header>
